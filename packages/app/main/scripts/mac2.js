20f5e24e37f75ed4d0d6e228b683763909704840
Tony Anziano <toanzian@microsoft.com>
2018-11-08 10:00:18 -0800

    Updated mac script.

--- packages/app/main/scripts/mac2.js
+++ packages/app/main/scripts/mac2.js
@@ -2,21 +2,41 @@ stageAndRedist().catch(e => console.error(e));
 
 /** Package the emulator using electron-builder */
 async function stageAndRedist() {
-  const packageJson = require('../package.json')
+  const common = require('../gulpfile.common.js');
+  const path = require('path');
+
+  /** Package the emulator using electron-builder */
+  const { getConfig } = common;
   const builder = require('electron-builder');
-  const config = packageJson.build;
+  const config = getConfig('mac', 'dir');
 
   // create build artifacts
   await builder.build({
     targets: builder.Platform.MAC.createTarget(['dir']),
-    config
+    config,
+    projectDir: path.resolve('.')
   });
 
+  /** Creates the emulator installers */
+  const config2 = getConfig('mac');
+
   // create installers
-  const results = await builder.build({
+  const filenames = await builder.build({
     targets: builder.Platform.MAC.createTarget(['zip', 'dmg']),
-    config,
-    prepackaged: './dist/mac'
+    config2,
+    prepackaged: '../dist/mac',
+    projectDir: path.resolve('.')
   });
-  console.log(results);
+  console.log(filenames);
+
+  // rename and move the files to the /dist/ directory
+  // await new Promise(resolve => {
+  //   gulp
+  //     .src(filenames, { allowEmpty: true })
+  //     .pipe(rename(path => {
+  //       path.basename = getReleaseFilename();
+  //     }))
+  //     .pipe(gulp.dest('./dist'))
+  //     .on('end', resolve);
+  // });
 };
